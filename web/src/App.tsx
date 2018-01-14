244246d323cbf21d65fdc05520ccaa070c993eae
walton <bigw369@gmail.com>
2018-01-14 17:15:14 -0500

    basic react state

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,16 +4,32 @@ import './App.css';
 const logo = require('./logo.svg');
 
 class App extends React.Component {
+
+  state = {
+    paid: false
+  };
+  
   render() {
+    let body;
+
+    if (this.state.paid) {
+      body = (
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/1JlRWdNAi7I?rel=0&amp;showinfo=0"
+        />);
+    } else {
+      body = <button onClick={() => this.setState({paid: true})} >Pay</button>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Bard</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {body}
       </div>
     );
   }
